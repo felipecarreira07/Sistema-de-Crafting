603c54fc365163ee9b3fcf605731aa2a1dfaa64c
felipecarreira07 <felipecarreira2020@gmail.com>
2026-08-27 18:09:00 -0300

    Fiz uma recipe com multiplos ingredientes e ta deu certo

--- main.py
+++ main.py
@@ -29,19 +29,33 @@ recipes = {
             ],
             "resultado": 2
         },
+        "motor_eletrico": {
+            "nome": "Motor Eletrico",
+            "ingredientes": [
+                {
+                    "nome": "placa_de_ferro",
+                    "qnt": 4    
+                },
+                {
+                    "nome": "fio_de_cobre",
+                    "qnt": 4    
+                },
+            ],
+            "resultado": 3
+        },
         
     }
 
 
 def calcular_recipes(recipe, qnt):
 
+    print('-----------------------------------------------------------')
     print(f'Para craftar {qnt} {recipe['nome']} voce vai precisar de: ')
     for ingrediente in recipe['ingredientes']:
         print(f"{ingrediente['nome']}: {ingrediente['qnt'] * (qnt / recipe['resultado'])}")
         
     print('-----------------------------------------------------------')
+    return
 
-
-
-teste = 'fio_de_cobre'
+teste = 'motor_eletrico'
 calcular_recipes(recipes[teste], 10)
